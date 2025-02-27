4c1c6500fecefd1bb63b0226a73e763ea1e3731e
Dave <dave@Daves-Mac-mini.local>
2025-02-27 07:25:50 -0800

    add ignore

---
+++ TipKit-Examples/TipKit-Examples/InlineTipView.swift
@@ -0,0 +1,18 @@
+//
+//  InlineTipView.swift
+//  TipKit-Examples
+//
+//  Created by Dave on 2/27/25.
+//
+
+import SwiftUI
+
+struct InlineTipView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InlineTipView()
+}
---
+++ TipKit-Examples/TipKit-Examples/TipKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipKit_ExamplesApp.swift
+//  TipKit-Examples
+//
+//  Created by Dave on 2/27/25.
+//
+
+import SwiftUI
+
+@main
+struct TipKit_ExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
